9742f1d64c74e9627e86a47aba053db39d430b5f
Roniel Nunes <46944438+RonielNunes@users.noreply.github.com>
2019-11-27 21:57:38 -0300

    implementação esboço main teste

---
+++ ListaEncadeada/main.c
@@ -0,0 +1,31 @@
+#include "TAD_texto.h"
+
+int main() {
+  int opcao = -1;
+
+  while (opcao!=0) {
+    printf("************************[MENU TEXTO]******************\n");
+    printf("* 1) - Inicializar texto                             *\n");
+    printf("* 2) - Inserir palvra Texto                          *\n");
+    printf("* 3) - Imprimir texto                                *\n");
+    printf("* 4) - Remover palvra no texto                       *\n");
+    printf("* 5) - Tamanho da do texto                           *\n");
+    printf("* 0) - Sair                                          *\n");
+    printf("******************************************************\n");
+    scanf("%d",&opcao);
+    if (opcao == 1) {
+
+    }else if(opcao == 2){
+
+    }else if(opcao == 3){
+
+
+    }else if(opcao == 4){
+
+    }else if (opcao == 5) {
+
+    }
+  }
+
+  return 0;
+}
